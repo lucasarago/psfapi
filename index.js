846bb5f18674227f1c82a7f3cd8b9e8988ec966a
Lucas Bohorquez <lbohorquez@aragoconsulting.com>
2021-04-10 13:25:23 -0500

    cors policy

--- index.js
+++ index.js
@@ -2,17 +2,19 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const router = express.Router();
 const employees = [];
 
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
+app.use(cors())
 
 router.get('/', (req, res) => {
-    res.status(200).send({ message: `API PSF V1 in OnLine`})
+    res.status(200).send({ message: `API PSF V1 is OnLine`})
 })
 
 router.get('/employees', (req, res) => {
